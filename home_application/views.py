ae4b5d26dff6e00d611cb36d11e00d31f61773ac
jiaquan <jiaquan@canway.net>
2019-07-06 11:19:02 +0800

    Initial commit

---
+++ home_application/views.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from common.mymako import render_mako_context
+
+
+def home(request):
+    """
+    首页
+    """
+    id = request.GET.get('id')
+    return render_mako_context(request, '/home_application/home.html',{ "id":id})
+
+
+def dev_guide(request):
+    """
+    开发指引
+    """
+    return render_mako_context(request, '/home_application/dev_guide.html')
+
+
+def contactus(request):
+    """
+    联系我们
+    """
+    return render_mako_context(request, '/home_application/contact.html')
